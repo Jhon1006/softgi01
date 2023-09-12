662ede859f0e64aa64ed89f6e693f2b4efb41e28
Eduar1981 <edcorgriz@hotmail.com>
2023-09-12 15:38:25 -0500

    mero css

---
+++ originalSoftgi01/ventas.py
@@ -0,0 +1,5 @@
+
+
+class factura:
+    def __init__(self, numero_factura, factura_cliente, factura_empleado, tipo_venta, fecha_venta, cantidad_producto, total_pagar, medio_pago, forma_pago) -> None:
+        pass
