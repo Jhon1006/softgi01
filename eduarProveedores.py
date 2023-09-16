4e76a703a9fdd23e60f06cec9970dd7fe2a23031
Eduar1981 <edcorgriz@hotmail.com>
2023-09-16 16:19:50 -0500

    def modificar y borrar proveedores

--- eduarProveedores.py
+++ eduarProveedores.py
@@ -1,27 +1,13 @@
  def modificar(self,proveedor):
-        sql=f"UPDATE proveedores SET tipo={proveedor[1]},docprov={proveedor[2]},={carrito[3]},modelo={carrito[5]},color='{carrito[4]}' WHERE placa='{carrito[0]}'"
+        sql=f"UPDATE proveedores SET tipo={proveedor[0]},docprov={proveedor[1]},nomprov={proveedor[2]},contprov={proveedor[3]},emaprov='{proveedor[4]}', direprov='{proveedor[5]}' WHERE docprov='{proveedor[0]}'"
         self.cursor.execute(sql)
         self.conexion.commit()
-        if carrito[6].filename != "":
-            # Subir la foto nueva
-            now = datetime.now()
-            tiempo = now.strftime("%Y%m%d%H%M%S")
-            nombre,extension = os.path.splitext(carrito[6].filename)
-            nuevoNombre = "F" + tiempo + extension
-            carrito[6].save("uploads/"+nuevoNombre)
-            # Consultar y borrar la foto vieja
-            sql = f"SELECT foto FROM carritos WHERE placa='{carrito[0]}'"
-            self.cursor.execute(sql)
-            fotoVieja = self.cursor.fetchall()
-            self.conexion.commit()
-            os.remove(os.path.join(self.programa.config['CARPETAU'],fotoVieja[0][0]))
-            # Actualizo nombre de la nueva foto en la db
-            sql = f"UPDATE carritos SET foto='{nuevoNombre}' WHERE placa='{carrito[0]}'"
-            self.cursor.execute(sql)
-            self.conexion.commit()
+        
+        self.cursor.execute(sql)
+        self.conexion.commit()
     
-    def borrar(self,pla):
-        sql=f"UPDATE carritos SET BORRADO=1 WHERE placa='{pla}'"
+    def borrar(self,docprov):
+        sql=f"UPDATE proveedores SET BORRADO=1 WHERE docprov='{docprov}'"
         self.cursor.execute(sql)
         self.conexion.commit()
         
