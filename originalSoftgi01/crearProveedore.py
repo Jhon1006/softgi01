521a330582cb8fe2afeec10c18fb6bad20701b08
Eduar1981 <edcorgriz@hotmail.com>
2023-09-17 14:03:54 -0500

    edu

--- originalSoftgi01/crearProveedore.py
+++ originalSoftgi01/crearProveedore.py
@@ -10,3 +10,5 @@ class creaProveedores:
         bsdsql = F" INSERT INTO `proveedores`(`docprov`, `nomprov`, `contprov`, `emaprov`, `direprov`) VALUES ('{agregar[0]}','{agregar[1]}','{agregar[2]}','{agregar[3]}','{agregar[4]}')"
         self.cursor.execute(bsdsql)
         self.conexion.commit()
+
+
